41b9d683e093118db516c528b056b64fe4206d37
MPermaul <moses.permaul@hotmail.com>
2019-08-29 22:26:41 -0400

    Commit final versions of assignement .py files.

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,35 +1,48 @@
 def listDivide (numbers, divide = 2):
-    '''Function that takes in a list (numbers) and return how many items are divisible by (divide).
+    '''Function that takes in list numbers and returns how many items are divisible by divide.
     Arguments:
-        numbers (list): Arg for a passed in list.
-        divide (int):   Arg for the value used to divide the list items by. Default is 2.
+        numbers (list):     Arg for a list that's passed in.
+        divide (int):       Arg for the value used to divide the list items by. Default is 2.
     
     Return:
         num_divisible (int):    The number of elements in list that are divisible by divide.
+
+    Example:
+        >>> listDivide([1,2,3,4,5])
+        >>> 2
+
+        >>> listDivide([30,54,63,98,100], divide=10)
+        >>> 2
+
     '''
     # variable to store how many items are divisible by divide
     num_divisible = 0 
     
     # loop to iterate through each item in list
-    for item in numbers:
+    for num in numbers:
         # increment num_visible if there is no remainder
-        if item % divide == 0:
+        if num % divide == 0:
                 num_divisible += 1
     
     return num_divisible
 
 class ListDivideException(Exception):
+    '''Custom Exception Class for a list divide error.'''
     pass
 
 def testListDivide():
-    '''Function that contains test scenarios for listDivide function
+    '''Function that contains test scenarios for listDivide function. Error raised if tests fail.'''
 
-    '''
-    listDivide([1,2,3,4,5]) #should return 2
-    listDivide([2,4,6,8,10]) #should return 5
-    listDivide([30, 54, 63,98, 100], divide=10) # should return 2
-    listDivide([]) # should return 0
-    listDivide([1,2,3,4,5], 1) # should return 5
+    # try to run each test scenario
+    try:
+        listDivide([1,2,3,4,5])
+        listDivide([2,4,6,8,10])
+        listDivide([30, 54, 63,98, 100], divide=10)
+        listDivide([])
+        listDivide([1,2,3,4,5], 1)
+    except:
+        # raise ListDivideException if there is an error
+        raise ListDivideException('Item in one of the lists is not an INT.')
 
 
 testListDivide()
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,17 @@
-
 class Book:
+    """This is a class tthat represents a book. 
+      
+    Attributes: 
+        author (string): Author of the book. 
+        title (string): The title of the book. 
+    """
 
     def __init__ (self, author, title):
         self.author = author
         self.title = title
 
     def display(self):
-        print('{}, written by {}.'.format(self.title,self.author))
+        print('\'{}\' written by {}'.format(self.title,self.author))
 
 
 book1 = Book('John Steinbeck','Of Mice and Men')
